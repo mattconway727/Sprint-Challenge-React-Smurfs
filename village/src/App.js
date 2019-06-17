8da0157743bfa469b9d1246b0c30bd735bb6a809
Matt Conway <mattconway727@gmail.com>
2019-06-17 18:25:29 -0400

    setup axios request using GET to /smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      errorMessage: null
     };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log(response);
+        this.setState({ smurfs: response.data, errorMessage: null });
+      })
+      .catch(err => {
+        this.setState({
+          errorMessage: "Error Fetching Smurfs! Check URL"
+        });
+        setTimeout(() => {
+          this.setState({ errorMessage: null });
+        }, 2000);
+      });
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -17,6 +34,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {this.state.errorMessage !== null ? (
+          <h3 style={{ color: "red" }}>{this.state.errorMessage}</h3>
+        ) : null}
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
